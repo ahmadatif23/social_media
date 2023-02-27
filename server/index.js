41d560f834813626847334f20d7b2d1a7e105fd3
ahmadatif23 <ahmadatif23@gmail.com>
2023-02-27 14:50:44 +0800

    post models and routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const morgan = require('morgan')
 // IMPORT ROUTES
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
+const postRoute = require('./routes/posts')
 
 dotenv.config()
 
@@ -23,6 +24,7 @@ app.use(morgan('common'))
 // ROUTES
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
 
 app.listen(8800, () => {
     console.log('Backend server is running.')
