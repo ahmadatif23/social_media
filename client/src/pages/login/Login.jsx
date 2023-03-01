15b7ce631055897b00b30ade4c9b6e85376957db
ahmadatif23 <ahmadatif23@gmail.com>
2023-03-01 16:15:52 +0800

    create authContext hooks

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,26 +1,36 @@
+import { useRef } from 'react'
 import './login.css'
 
 export default function Login() {
-  return (
-    <div className='login'>
-        <div className="loginWrapper">
-            <div className="loginLeft">
-                <h3 className="loginLogo">socialLah</h3>
-                <span className="loginDesc">
-                    Connect with friends and the world arounf you with socialLah.
-                </span>
-            </div>
+    const email = useRef()
+    const password = useRef()
 
-            <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder='Email' className="loginInput" />
-                    <input placeholder='Password' className="loginInput" />
-                    <button className="loginButton">Log In</button>
-                    <span className="loginForgot">Forgot Password?</span>
-                    <button className="loginRegisterButton">Create a New Account</button>
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email.current.value)
+        console.log(password.current.value)
+    }
+
+    return (
+        <div className='login'>
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">socialLah</h3>
+                    <span className="loginDesc">
+                        Connect with friends and the world arounf you with socialLah.
+                    </span>
+                </div>
+
+                <div className="loginRight">
+                    <form className="loginBox" onSubmit={ handleSubmit }>
+                        <input required ref={ email } placeholder='Email' type='email' className="loginInput" />
+                        <input required ref={ password } minLength="6" placeholder='Password' type='password' className="loginInput" />
+                        <button className="loginButton">Log In</button>
+                        <span className="loginForgot">Forgot Password?</span>
+                        <button className="loginRegisterButton">Create a New Account</button>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
